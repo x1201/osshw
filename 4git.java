9c38bb56348d9737651c9b9ad7090eabc70ea745
Alice <Alice@localhost.localdomain>
2018-10-28 21:05:32 +0900

    commit 3

--- 4git.java
+++ 4git.java
@@ -2,5 +2,6 @@ public class 4git {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
       System.out.println("Hello, world from GIT!2");
+      System.out.println("Hello, world from GIT!3");
    }
 }
